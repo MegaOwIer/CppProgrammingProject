38f797ac5ed9e2903fa15bcb19cb1482e4f3287f
MegaOwIer <690601124@qq.com>
2020-05-06 17:38:16 +0800

    Remove useless shell scripts.

--- analyze.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-MyPass=/home/metazero/code/VS-Code-CPP/Project/MyPass.so
-clang -Xclang -load -Xclang $MyPass -c $1
--- dot.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-dot -Tpng -o $1.png $1.dot
-dot -Tpng -o $1.flat.png $1.flat.dot
-dot -Tpng -o $1.transaction.png $1.transaction.dot
